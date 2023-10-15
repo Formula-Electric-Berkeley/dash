c34e14dd9482b3be475614f920ade6293c9f4233
Raadwan Masum <piraadwan@gmail.com>
2023-10-15 15:34:37 -0700

    create storage route

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", all_run_data=db.get_all_run_data())
+    return render_template("index.html")
 
+
+@app.route("/storage")
+def storage():
+    return render_template("storage.html", all_run_data=db.get_all_run_data())
 
 if __name__ == "__main__":
-    app.run(threaded=True, use_reloader=True, debug=True)
+    app.run(port=8000, threaded=True, use_reloader=True, debug=True)
