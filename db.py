3ab03f2547f8c9f8325d3b7296e0602e04f3de55
Raadwan Masum <piraadwan@gmail.com>
2023-10-15 14:05:58 -0700

    format db.py

--- db.py
+++ db.py
@@ -4,14 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def get_database():
-   CONNECTION_STRING = os.getenv("ATLAS_URI")
+    CONNECTION_STRING = os.getenv("ATLAS_URI")
 
-   client = MongoClient(CONNECTION_STRING)
- 
-   return client['run_data']
-  
-if __name__ == "__main__":   
-   dbname = get_database()
+    client = MongoClient(CONNECTION_STRING)
 
-   print(dbname)
+    return client["run_data"]
+
+
+if __name__ == "__main__":
+    dbname = get_database()
+
+    print(dbname)
