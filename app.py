364ca9321508adeea57c6c4c6087e8b26619eb23
Raadwan Masum <piraadwan@gmail.com>
2023-10-15 23:00:50 -0700

    add routing for 404 page

--- app.py
+++ app.py
@@ -19,5 +19,10 @@ def storage():
     return render_template("storage.html", all_run_data=db.get_all_run_data())
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+
 if __name__ == "__main__":
     app.run(port=8000, threaded=True, use_reloader=True, debug=True)
