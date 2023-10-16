db902783b649a5e9afe29f8105d8b92fd4b59e74
Raadwan Masum <piraadwan@gmail.com>
2023-10-15 17:46:15 -0700

    format app.py

--- app.py
+++ app.py
@@ -8,13 +8,16 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/analysis")
 def analysis():
     return render_template("analysis.html")
 
+
 @app.route("/storage")
 def storage():
     return render_template("storage.html", all_run_data=db.get_all_run_data())
+
 
 if __name__ == "__main__":
     app.run(port=8000, threaded=True, use_reloader=True, debug=True)
