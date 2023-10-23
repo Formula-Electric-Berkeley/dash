04fc4153571c4093a24ef75f74ebdcd96639cacd
Raadwan Masum <piraadwan@gmail.com>
2023-10-23 15:20:32 -0700

    use new db structure

--- db.py
+++ db.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 CONNECTION_STRING = os.getenv("ATLAS_URI")
-run_data_db = MongoClient(CONNECTION_STRING)["run_data"]
-run_data_collection = run_data_db.records
+dash_db = MongoClient(CONNECTION_STRING)["dash_db"]
+dash_data_collection = dash_db.data
+dash_headers_collection = dash_db.headers
 
 
 def get_all_run_data():
     """Returns all run data documents in run_data collection"""
-    return run_data_collection.find()
+    return None
