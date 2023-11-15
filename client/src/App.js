e6695c3d76a3781ca055cf31a7b2bc777eb79f6d
Raadwan Masum <piraadwan@gmail.com>
2023-11-14 20:49:11 -0800

    setup components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import Landing from './components/landing/landing.js'
+import './index.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" exact element={<Landing />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ client/src/components/landing/landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    console.log(":LKSDJF:LSDKJFFL:KJD")
+    return (
+        <div>
+            <h1>DASH</h1>
+        </div>
+    );
+};
+
+export default Landing;
