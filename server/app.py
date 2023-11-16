3f9cd0eacb0243dec4f7a3a00369e947bbebc2d1
Raadwan Masum <piraadwan@gmail.com>
2023-11-15 20:04:48 -0800

    remove old server code

--- server/app.py
+++ server/app.py
@@ -1,12 +1,6 @@
-import datetime
 import os
-import bson
-import db
-import parsing
-from bson.json_util import dumps, loads
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request
 from werkzeug.utils import secure_filename
-from bson.objectid import ObjectId
 from flask_cors import CORS, cross_origin
 
 ALLOWED_EXTENSIONS = {"csv"}
@@ -19,23 +13,6 @@ app.config["UPLOAD_FOLDER"] = "./uploads/"
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-@app.route("/graph", methods=["POST"])
-def graph():
-    source_id = request.json["source"]
-    device = request.json["device"]
-    parameter = request.json["parameter"]
-
-    source_document = db.dash_data_collection.find_one(
-        {"_id": ObjectId(source_id)})
-
-    graph_data = {
-        "timestamps": source_document["timestamps"], "data": source_document[parameter]}
-    source_name = source_document["name"]
-    graph_data["name"] = f"{source_name} {device} {parameter}"
-
-    return graph_data
 
 
 @app.route("/upload", methods=["GET", "POST", "OPTIONS"])
@@ -58,88 +35,6 @@ def upload():
             file.save(filepath)
 
         return "SUCCESS"
-
-
-@app.route("/storage", methods=["GET", "POST"])
-def storage():
-    if request.method == "POST":
-        if "file" not in request.files:
-            flash("No file part")
-            return redirect(request.url)
-        file = request.files["file"]
-
-        if file.filename == "":
-            flash("No selected file")
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-
-            file.save(filepath)
-
-            data_header = {
-                "name": request.form["title"],
-                "filename": filename,
-                "date": datetime.date.today().strftime("%x"),
-            }
-
-            data = parsing.parse_csv(filepath)
-            # data = parsing.parse_csv_dynamics(filepath)
-
-            insert_header_result = db.dash_headers_collection.insert_one(
-                data_header)
-
-            data_header.update(data)
-            data_header.update({"_id": insert_header_result.inserted_id})
-
-            db.dash_data_collection.insert_one(data_header)
-
-            db.dash_headers_collection.update_one(
-                {
-                    "_id": insert_header_result.inserted_id,
-                },
-                {
-                    "$set": {
-                        "size": round(
-                            len(
-                                bson.BSON.encode(
-                                    db.dash_data_collection.find_one(
-                                        {"_id": insert_header_result.inserted_id}
-                                    )
-                                )
-                            )
-                            / 1e6,
-                            1,
-                        ),
-                        "devices": db.dash_data_collection.find_one(
-                            {"_id": insert_header_result.inserted_id}
-                        )["devices"],
-                        "parameters": db.dash_data_collection.find_one(
-                            {"_id": insert_header_result.inserted_id}
-                        )["parameters"],
-                    },
-                },
-            )
-
-            os.remove(filepath)
-
-            return redirect("/storage")
-
-    return render_template(
-        "storage.html",
-        data_headers=db.dash_headers_collection.find(),
-        storage_size=round(db.dash_db.command("dbstats")["dataSize"] / 1e6, 1),
-        storage_size_percent=round(
-            (db.dash_db.command("dbstats")["dataSize"] / 1e6) / 512 * 100, 2
-        ),
-    )
-
-
-@app.route("/delete_document/<id>", methods=["DELETE"])
-def delete(id):
-    db.dash_headers_collection.delete_one({"_id": ObjectId(id)})
-    db.dash_data_collection.delete_one({"_id": ObjectId(id)})
-    return f"Sucessfully deleted document with id: {id}"
 
 
 @app.errorhandler(404)
