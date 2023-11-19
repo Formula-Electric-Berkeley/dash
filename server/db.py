116c215376e11dde5926efb51e139658b136b0b9
Raadwan Masum <piraadwan@gmail.com>
2023-11-19 12:17:32 -0800

    insert data into cdb table

--- server/db.py
+++ server/db.py
@@ -20,20 +20,31 @@ def add_file(filename, filepath, size, uploadDate):
 
     cursor.execute(
         f"INSERT INTO files (id, filename, size, uploadDate) VALUES ('{unique_id}', '{filename}', {size}, '{uploadDate}')")
-
     conn.commit()
 
     df = pd.read_csv(filepath)
     df_columns = list(df.columns)
     table_str = ""
+    insert_str = ""
     for i in df_columns:
         column_name = ''.join(char for char in i if char.isalnum())
         table_str += "col" + column_name + " STRING, "
+        insert_str += "col" + column_name + ", "
     table_str = table_str[:-2]
+    insert_str = insert_str[:-2]
 
     cursor.execute(
         f"CREATE TABLE {unique_id} ({table_str})")
+    conn.commit()
 
+    for row in df.itertuples():
+        row_list = list(row)[1:]
+        row_str = ', '.join(f"'{x}'" for x in row_list)
+
+        cursor.execute(f'''
+               INSERT INTO {unique_id} ({insert_str})
+               VALUES ({row_str})
+               ''')
     conn.commit()
 
 
