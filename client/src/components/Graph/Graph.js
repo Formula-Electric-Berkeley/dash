c895f07a5db410c30fdfa9bea8368f83088eaae0
Raadwan Masum <piraadwan@gmail.com>
2023-11-19 18:56:26 -0800

    create scatter plot component

--- client/src/components/Graph/Graph.js
+++ client/src/components/Graph/Graph.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ScatterPlot from './ScatterPlot';
 
 const Graph = () => {
     return (
         <div>
-        GRAPH
+            <ScatterPlot/>
         </div>
     );
 };
---
+++ client/src/components/Graph/ScatterPlot.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ScatterPlot = () => {
+    return (
+        <div>
+            <h1>SCATTER PLOT</h1>
+        </div>
+    );
+};
+
+export default ScatterPlot;
