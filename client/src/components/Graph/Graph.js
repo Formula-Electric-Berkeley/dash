2df61d8936ce4682b4a48b5bc90b5adc7e15b0dc
Raadwan Masum <piraadwan@gmail.com>
2023-11-19 19:14:02 -0800

    style scatter plot

--- client/src/components/Graph/Graph.js
+++ client/src/components/Graph/Graph.js
@@ -3,7 +3,7 @@ import ScatterPlot from './ScatterPlot';
 
 const Graph = () => {
     return (
-        <div>
+        <div className='w-full h-full'>
             <ScatterPlot/>
         </div>
     );
--- client/src/components/Graph/ScatterPlot.js
+++ client/src/components/Graph/ScatterPlot.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
 
 const ScatterPlot = () => {
+    let data = [
+        {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'markers',
+        },
+    ]
+
+    let layout = {
+        autosize: true,
+        font: {
+            family: 'Inter, sans-serif',
+            size: 16,
+            color: '#fff'
+        },
+        paper_bgcolor: "#000000",
+        plot_bgcolor: "#000000",
+        title: {
+            text: 'TITLE',
+        },
+        yaxis: {
+            title: {
+                text: 'VALUES',
+            },
+            zerolinecolor: "#fff",
+            gridcolor: "gray",
+            griddash: 'dot',
+        },
+        xaxis: {
+            title: {
+                text: 'TIME',
+            },
+            zeroline: false,
+        },
+        legend: {
+            x: 1,
+            xanchor: 'right',
+            y: 1
+        }
+    }
+
+    let config = {
+    }
+
     return (
-        <div>
-            <h1>SCATTER PLOT</h1>
+        <div className='w-full h-full p-5'>
+            <Plot className='w-full h-full' data={data} layout={layout} config={config} />
         </div>
     );
 };
