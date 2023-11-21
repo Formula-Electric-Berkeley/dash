2803847c3e71a98efc2d78deb5b6de1bca457461
Raadwan Masum <piraadwan@gmail.com>
2023-11-21 15:10:43 -0800

    create selection box for graphs

--- client/src/components/Graph/Graph.js
+++ client/src/components/Graph/Graph.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ScatterPlot from './ScatterPlot';
 
 const Graph = () => {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }))
+    }
+
+    const handleConfigureGraph = (event) => {
+        event.preventDefault();
+        console.log(inputs);
+    }
+
     return (
-        <div className='w-full h-full'>
-            <ScatterPlot/>
+        <div className='w-full h-full flex justify-center align-middle'>
+            <div className='max-w-lg w-4/5 h-4/6 max-h-96 m-auto bg-zinc-800 rounded-3xl'>
+                <h1>Configure Visualization</h1>
+                <form onSubmit={handleConfigureGraph}>
+                    <select name='graphType' onChange={handleChange}>
+                        <option value="scatter">Scatter Plot</option>
+                        <option value="line">Line Graph</option>
+                    </select>
+                    <select name='xAxis' onChange={handleChange}>
+                        <option value="scatter">Scatter Plot</option>
+                        <option value="line">Line Graph</option>
+                    </select>
+                    <select name='yAxis' onChange={handleChange}>
+                        <option value="scatter">Scatter Plot</option>
+                        <option value="line">Line Graph</option>
+                    </select>
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
