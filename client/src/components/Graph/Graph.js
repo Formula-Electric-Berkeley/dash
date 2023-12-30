df7f7c6de359f871fd205b4f4b8c3971b79fab08
Raadwan Masum <piraadwan@gmail.com>
2023-12-29 23:06:49 -0800

    create new graph form structure (redo this)

--- client/src/components/Graph/Graph.js
+++ client/src/components/Graph/Graph.js
@@ -2,15 +2,13 @@ import React, { useState, useEffect, useContext } from 'react';
 import { DataIdContext } from '../../App';
 import './Graph.css';
 import Loading from '../Utils/Loading';
-import Multiselect from 'multiselect-react-dropdown';
 import ScatterPlot from './ScatterPlot';
+import Select from 'react-select';
 
 const Graph = () => {
     const { dataId, setDataId } = useContext(DataIdContext);
 
-    const [xAxisElements, setXAxisElements] = useState([]);
-    const [yAxisOptions, setYAxisOptions] = useState([]);
-    const [inputs, setInputs] = useState({});
+    const [axisElements, setAxisElements] = useState([]);
 
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -22,15 +20,12 @@ const Graph = () => {
         })
             .then((response) => response.json())
             .then((data) => {
-                let currXElements = []
-                let currYElements = []
+                let currElements = []
                 for (const entry of data) {
-                    let columnName = entry[0].substring(3);
-                    currXElements.push(<option value={columnName}>{columnName}</option>)
-                    currYElements.push({ name: columnName })
+                    let columnName = entry.substring(3);
+                    currElements.push({ value: columnName, label: columnName })
                 }
-                setXAxisElements(currXElements)
-                setYAxisOptions(currYElements)
+                setAxisElements(currElements)
                 setLoading(false);
             })
             .catch((error) => {
@@ -41,30 +36,8 @@ const Graph = () => {
 
     }, [dataId]);
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value }))
-    }
-
-    const handleConfigureGraph = (event) => {
-        event.preventDefault();
-        setShowGraph(true);
-        console.log(inputs.xAxis)
-    }
-
-    const onSelect = (selectedList, selectedItem) => {
-        const name = "yAxis"
-        setInputs(values => ({ ...values, [name]: selectedList }))
-    }
-
-    const onRemove = (selectedList, removedItem) => {
-        const name = "yAxis"
-        setInputs(values => ({ ...values, [name]: selectedList }))
-    }
-
     if (loading) {
-        return <Loading/>;
+        return <Loading />;
     }
 
     if (error) {
@@ -78,39 +51,51 @@ const Graph = () => {
                     <div className='max-w-lg w-4/5 m-auto p-10 
                 bg-zinc-800 rounded-3xl'>
                         <h1 className='text-2xl'>Configure Graph</h1>
-                        <form onSubmit={handleConfigureGraph}
-                            className='text-black'>
-                            <div className='w-full flex flex-col'>
-                                <select name='graphType' onChange={handleChange}
-                                    className='w-full my-5 py-1 px-2 rounded-lg outline-none'>
-                                    <option value="" disabled selected>Select Graph Type</option>
-                                    <option value="scatter">Scatter Plot</option>
-                                </select>
-                                <select name='xAxis' onChange={handleChange}
-                                    className='w-full mb-5 py-1 px-2 rounded-lg outline-none'>
-                                    <option value="" disabled selected>Select X-Axis</option>
-                                    {xAxisElements}
-                                </select>
-                                <Multiselect
-                                    options={yAxisOptions}
-                                    className='mb-5 rounded-lg outline-none'
-                                    onSelect={onSelect}
-                                    onRemove={onRemove}
-                                    placeholder='Select Y-Axis'
-                                    displayValue="name"
-                                />
-                                <button className='m-auto px-10 py-1 bg-yellow
-                                hover-shadow-yellow rounded-full text-black
-                                font-bold cursor-pointer'
-                                    type='submit'>
-                                    Generate Graph</button>
-                            </div>
-                        </form>
+
+                        <Select
+                            className='my-5'
+                            styles={{
+                                option: (baseStyles, state) => ({
+                                    ...baseStyles,
+                                    color: 'black',
+                                    backgroundColor: state.isFocused || state.isSelected ? '#fbb414' : null,
+                                }),
+                            }}
+                            options={[{ value: 'scatter', label: 'Scatter Plot' },
+                            { value: 'line', label: 'Line Chart' }]}
+                            placeholder={<div>Graph Type</div>}
+                        />
+
+                        <Select
+                            className='my-5'
+                            styles={{
+                                option: (baseStyles, state) => ({
+                                    ...baseStyles,
+                                    color: 'black',
+                                    backgroundColor: state.isFocused || state.isSelected ? '#fbb414' : null,
+                                }),
+                            }}
+                            options={axisElements}
+                            placeholder={<div>X-Axis</div>}
+                        />
+
+                        <Select
+                            className='my-5'
+                            styles={{
+                                option: (baseStyles, state) => ({
+                                    ...baseStyles,
+                                    color: 'black',
+                                    backgroundColor: state.isFocused || state.isSelected ? '#fbb414' : null,
+                                }),
+                            }}
+                            options={axisElements}
+                            placeholder={<div>Y-Axis</div>}
+                        />
                     </div>
                 </div>
             )}
-            {showGraph &&
-                <ScatterPlot xColumnName={inputs.xAxis} yColumnArray={inputs.yAxis} />
+            {//showGraph &&
+                //  <ScatterPlot xColumnName={inputs.xAxis} yColumnArray={inputs.yAxis} />
             }
         </div>
     );
