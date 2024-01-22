0f026c6feeb97ac58778cef0349aea76bf17ee45
Raadwan Masum <piraadwan@gmail.com>
2024-01-22 15:34:59 -0800

    style loading animation

--- client/src/components/Utils/Loading.js
+++ client/src/components/Utils/Loading.js
@@ -3,9 +3,9 @@ import './Loading.css';
 
 const Loading = () => {
     return (
-        <div className="loading-box">
-            <div className="road">
-                <h1>LOADING...</h1>
+        <div className="loading-box h-full">
+            <div className="road h-full">
+                <h1 className='top-8'>LOADING...</h1>
                 <div className="taxi">
                     <div className="light"></div>
                     <span>
